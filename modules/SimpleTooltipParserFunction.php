944063535d49bb1bce7769a6e60feee4bf412bf7
Fannon <heimlersimon@gmail.com>
2015-01-21 11:21:17 +0100

    {{#simple-tooltip-info parser function implemented

--- modules/SimpleTooltipParserFunction.php
+++ modules/SimpleTooltipParserFunction.php
@@ -46,14 +46,11 @@ class SimpleTooltipParserFunction {
      */
     public static function infoTooltip( $parser, $value /* arg2, arg3, */ ) {
 
-        $args = array_slice( func_get_args(), 2 );
-        $title = $args[0] || '';
-
         //////////////////////////////////////////
         // BUILD HTML                           //
         //////////////////////////////////////////
 
-        $html = '<span class="simple-tooltip simple-tooltip-inline" title="' . htmlspecialchars($title) . '">' . htmlspecialchars($value) . '</span>';
+        $html = '<span class="simple-tooltip simple-tooltip-info" title="' . htmlspecialchars($value) . '" src=""></span>';
 
         return array(
             $html,
