d6c711e677f8e3d3479c06b963dc206d39e8416e
gesinn-it-ilm <gesinn-it-ilm@gesinn.it>
2024-06-11 12:59:57 +0200

    fix sniff Generic.WhiteSpace.DisallowSpaceIndent reported by phpcs

--- tests/phpunit/Unit/SimpleTooltipHooksTest.php
+++ tests/phpunit/Unit/SimpleTooltipHooksTest.php
@@ -11,7 +11,6 @@
 
 namespace Tests\Unit;
 
-use MediaWiki\MediaWikiServices;
 use PHPUnit\Framework\TestCase;
 use Parser;
 use SimpleTooltipHooks;
@@ -22,51 +21,51 @@ use SimpleTooltipHooks;
  */
 class SimpleTooltipHooksTest extends TestCase {
 
-    public function testInlineTooltip() {
-        // Create a mock for the Parser class
-        $parserMock = $this->getMockBuilder(Parser::class)
-                            ->disableOriginalConstructor()
-                            ->getMock();
+	public function testInlineTooltip() {
+		// Create a mock for the Parser class
+		$parserMock = $this->getMockBuilder(Parser::class)
+							->disableOriginalConstructor()
+							->getMock();
 
-        // Set up the mock behavior for recursiveTagParseFully
-        $parserMock->expects($this->once())
-                    ->method('recursiveTagParseFully')
-                    ->willReturn('Tooltip content');
+		// Set up the mock behavior for recursiveTagParseFully
+		$parserMock->expects($this->once())
+					->method('recursiveTagParseFully')
+					->willReturn('Tooltip content');
 
-        // Call the inlineTooltip method with some test values
-        $tooltip = SimpleTooltipHooks::inlineTooltip($parserMock, 'Inline text', 'Tooltip text');
+		// Call the inlineTooltip method with some test values
+		$tooltip = SimpleTooltipHooks::inlineTooltip($parserMock, 'Inline text', 'Tooltip text');
 
-        // Assert that the returned HTML contains the expected tooltip content
-        $expectedHtml = '<span class="simple-tooltip simple-tooltip-inline" data-simple-tooltip="Tooltip content">Inline text</span>';
-        $this->assertEquals($expectedHtml, $tooltip[0]);
+		// Assert that the returned HTML contains the expected tooltip content
+		$expectedHtml = '<span class="simple-tooltip simple-tooltip-inline" data-simple-tooltip="Tooltip content">Inline text</span>';
+		$this->assertEquals($expectedHtml, $tooltip[0]);
 
-    }
+	}
 
-    public function testInfoTooltip() {
-        // Create a mock for the Parser class
-        $parserMock = $this->getMockBuilder(Parser::class)
-                            ->disableOriginalConstructor()
-                            ->getMock();
+	public function testInfoTooltip() {
+		// Create a mock for the Parser class
+		$parserMock = $this->getMockBuilder(Parser::class)
+							->disableOriginalConstructor()
+							->getMock();
 
-        // Call the infoTooltip method with some test values
-        $tooltip = SimpleTooltipHooks::infoTooltip($parserMock, 'Tooltip text');
+		// Call the infoTooltip method with some test values
+		$tooltip = SimpleTooltipHooks::infoTooltip($parserMock, 'Tooltip text');
 
-        // Assert that the returned HTML contains the expected tooltip content
-        $expectedHtml = '<span class="simple-tooltip simple-tooltip-info" data-simple-tooltip="Tooltip text"></span>';
-        $this->assertEquals($expectedHtml, $tooltip[0]);
-    }
+		// Assert that the returned HTML contains the expected tooltip content
+		$expectedHtml = '<span class="simple-tooltip simple-tooltip-info" data-simple-tooltip="Tooltip text"></span>';
+		$this->assertEquals($expectedHtml, $tooltip[0]);
+	}
 
-    public function testImgTooltip() {
-        // Create a mock for the Parser class
-        $parserMock = $this->getMockBuilder(Parser::class)
-                           ->disableOriginalConstructor()
-                           ->getMock();
+	public function testImgTooltip() {
+		// Create a mock for the Parser class
+		$parserMock = $this->getMockBuilder(Parser::class)
+						   ->disableOriginalConstructor()
+						   ->getMock();
 
-        // Call the imgTooltip method with some test values
-        $tooltip = SimpleTooltipHooks::imgTooltip($parserMock, 'Image URL', 'Tooltip text');
+		// Call the imgTooltip method with some test values
+		$tooltip = SimpleTooltipHooks::imgTooltip($parserMock, 'Image URL', 'Tooltip text');
 
-        // Assert that the returned HTML contains the expected tooltip content
-        $expectedHtml = '<img class="simple-tooltip simple-tooltip-img" data-simple-tooltip="Tooltip text" src="Image URL"></img>';
-        $this->assertEquals($expectedHtml, $tooltip[0]);
-    }
+		// Assert that the returned HTML contains the expected tooltip content
+		$expectedHtml = '<img class="simple-tooltip simple-tooltip-img" data-simple-tooltip="Tooltip text" src="Image URL"></img>';
+		$this->assertEquals($expectedHtml, $tooltip[0]);
+	}
 }
